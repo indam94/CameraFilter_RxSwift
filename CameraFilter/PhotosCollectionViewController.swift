7109161d2ee9380b4528ea0da2fb7f0b5eeef4c9
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-17 15:20:11 -0700

    Passing Selected Photo

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 import Photos
+import RxSwift
 
 class PhotosCollectionViewController : UICollectionViewController{
+    
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
     
     private var images = [PHAsset]()
     
@@ -47,6 +53,27 @@ class PhotosCollectionViewController : UICollectionViewController{
         }
         
         return cell
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let selectedAsset = self.images[indexPath.row]
+        
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFill, options: nil){
+            [weak self] image, info in
+            
+            guard let info = info else{return}
+            
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage{
+                if let image = image{
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
         
     }
     
