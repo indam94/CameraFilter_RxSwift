a15c998b2c591f3ee31604779856e128eadef2c5
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-17 15:10:10 -0700

    Displaying Photos from Photo Library

---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by DONGGUN LEE on 9/17/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell :UICollectionViewCell{
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+}
